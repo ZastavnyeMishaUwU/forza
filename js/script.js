f2c3460d69a82932a5f9bd3ec01bcf1d7c41ae16
xlibnui_makix <103821356+ZastavnyeMishaUwU@users.noreply.github.com>
2024-05-19 10:19:56 +0300

    Merge remote-tracking branch 'origin/master'

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("artemka lubit kurku gril!")
+
+
+let docTitle = document.title;
+window.addEventListener("blur", () =>{
+    document.title = "Come back!";
+})
+window.addEventListener("focus", () =>{
+    document.title = docTitle;
+})
+
